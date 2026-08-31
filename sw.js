f4bc82d093890d333ddf7e210b9a461b53c68404
GokedaLokesh <lokeshgokeda551@gmail.com>
2026-08-31 18:47:24 +0530

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-const CACHE="vizag-rise-v3";
-const CORE=["./","./index.html","./manifest.webmanifest","./css/main.css","./css/menu.css","./css/hud.css","./css/map.css","./js/main.js","./js/core/ReleaseManager.js"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",event=>{
- if(event.request.method!=="GET")return;
- const url=new URL(event.request.url);
- if(url.origin===location.origin){
-   event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html"))));
- }
-});
+const CACHE="vizag-rise-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./css/main.css","./css/menu.css","./css/hud.css","./css/map.css"]))));self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const c=n.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return n}).catch(()=>caches.match("./index.html"))))})
